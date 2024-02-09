742f73c5ec65e8f58188a0179e852b56a3b86407
Facundo Recabarren <facundorecabarren10@gmail.com>
2024-02-08 21:11:11 -0300

    Update

--- portfolio/src/components/Hero/Hero.jsx
+++ portfolio/src/components/Hero/Hero.jsx
@@ -18,10 +18,10 @@ const Hero = () => {
   },[])
 
   return (
-    <div data-aos="fade" id="home" className="h-screen flex flex-col justify-center items-center gap-10 bg-background sm:flex-row sm:px-10 pt-16 sm:pt-0">
+    <div data-aos="fade" id="home" className="h-screen flex flex-col justify-center items-center gap-10 bg-background sm:flex-row sm:px-10 pt-20 sm:pt-0">
       <img src="https://res.cloudinary.com/dreso9ye9/image/upload/v1707401291/Portfolio%20FR/Polygon_Luminary_4_aiou4p.svg" alt="" className="absolute z-0 h-screen w-screen object-cover"/>
         <section className="flex flex-col justify-center items-center gap-8 xl:gap-12 sm:w-1/2 z-10">
-            <h1 id="titleDeveloper" className="text-center font-extrabold text-6xl lg:text-7xl xl:text-[5.5rem] text-palette-500 m-0">Full Stack Web Developer</h1>
+            <h1 id="titleDeveloper" className="text-center font-extrabold text-5xl lg:text-7xl xl:text-[5.5rem] text-palette-500 m-0">Full Stack Web Developer</h1>
             <h2 className="text-center text-palette-200 font-light xl:text-2xl xl:px-8 px-5">{t("description")}</h2>
             <div className="flex justify-center items-center gap-3">
                 <IoLogoJavascript title="JavaScript" className="text-2xl lg:text-4xl cursor-pointer hover:animate-rotate-center-opacity text-palette-500"/>
@@ -37,7 +37,7 @@ const Hero = () => {
             </div>
         </section>
         <section className="z-10">
-            <img src="https://res.cloudinary.com/dreso9ye9/image/upload/v1683905409/Facu_Bariloche-7_ypts38-Instagram_feed_q8xbuf.jpg" alt="" className="rounded-full w-[23rem] xl:w-[30rem]"/>
+            <img src="https://res.cloudinary.com/dreso9ye9/image/upload/v1683905409/Facu_Bariloche-7_ypts38-Instagram_feed_q8xbuf.jpg" alt="" className="rounded-full w-[20rem] sm:w-[25rem] xl:w-[30rem]"/>
         </section>
         <SlArrowDown className="hidden sm:flex absolute bottom-[10rem] lg:hidden text-5xl text-palette-500 animate-bounce"/>
     </div>
